08d58f625ca5c3955ed49d8f3ba91c4fa3cd0c4f
이창훈 <73262261+leechanghoon3024@users.noreply.github.com>
2021-04-13 15:14:14 +0900

    d

--- src/Video.js
+++ src/Video.js
@@ -1,6 +1,7 @@
 import Vimeo from "@u-wave/react-vimeo";
 import {useCallback, useEffect, useState} from "react";
-
+import ReactPlayer from 'react-player'
+import './App.css';
 
 const Video = ({match, location}) => {
     console.log(match.params.id)
@@ -29,21 +30,31 @@ const Video = ({match, location}) => {
     return (
         <div scroll="no" style={ {  overflow: 'hidden', } }>
             {id !== -1 ? (
-                <Vimeo
-                    style={{
-                        width : '100vw', height : '100vh'
-                    }}
-                    onError={onError}
-                    video={id}
-                    autoplay={true}
-                    responsive
-                    loop
-                    showByline={false}
-                    showTitle={false}
-                    controls={hide}
-                    color={'FF4C22'}
-                    showPortrait={false}
-                />
+                // <Vimeo
+                //     style={{
+                //         width : '100vw', height : '100vh'
+                //     }}
+                //     onError={onError}
+                //     video={id}
+                //     autoplay={true}
+                //     responsive
+                //     loop
+                //     showByline={false}
+                //     showTitle={false}
+                //     controls={hide}
+                //     color={'FF4C22'}
+                //     showPortrait={false}
+                // />
+                <div className='player-wrapper'>
+
+                    <iframe frameBorder="0" width="100%" height="100%"
+                            className='react-player'
+                            allow={'autoplay; fullscreen; picture-in-picture'}
+                            src={`https://player.vimeo.com/video/${id}?title=0&byline=0&portrait=0&speed=0&color=ff4c22&autoplay=1&loop=1`}
+                            webkitAllowFullScreen={true}
+                            mozAllowFullScreen={true}
+                            allowFullScreen={true}/>
+                </div>
             )
             :
                 (
