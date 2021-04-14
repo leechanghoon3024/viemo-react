28b365d0f92c71edad6843086433fe4e65eaa45b
이창훈 <73262261+leechanghoon3024@users.noreply.github.com>
2021-04-14 17:01:25 +0900

    d

--- src/Video.js
+++ src/Video.js
@@ -1,72 +1,92 @@
 import Vimeo from "@u-wave/react-vimeo";
-import {useCallback, useEffect, useState} from "react";
+import {useCallback, useEffect, useRef, useState} from "react";
 import ReactPlayer from 'react-player'
 import './App.css';
+import fscreen from 'fscreen';
 
 const Video = ({match, location}) => {
-    console.log(match.params.id)
-    console.log(location)
+
     const [ id, setId] = useState(-1)
     const [ hide, setHide] = useState(true)
+    const handleRef = useRef(null);
+    const [play, setPlay] = useState(false)
+
+
     useEffect(()=>{
-        window.addEventListener('resize', isHiding)
-        document.addEventListener('webkitfullscreenchange', onFullScreen)
-        document.addEventListener("fullscreenchange", onFullScreen);
-        document.addEventListener("mozfullscreenchange", onFullScreen);
-    },[])
+        if(window.ReactNativeWebView) {
+            window.ReactNativeWebView.postMessage('change') // is working
+        }
+    },[id, play])
+
+    const test = () => {
+        console.log("test change") // web working
+        if(window.ReactNativeWebView){
+            window.ReactNativeWebView.postMessage('change') // but this not working
+        }
+    }
 
     useEffect(()=>{
         const vimeo = match.params?.id ?? 532522978
         setId(vimeo)
     },[match.params.id])
 
-    const isHiding = () => {
-        setHide(window.innerWidth >= 520)
-        console.log(window.innerWidth >= 520)
-    }
-    const onError = (e) =>{
-        setId(-1)
-        console.log("eeee", e)
-
-    }
 
 
-    const onFullScreen = (e) => {
-        const isFullscreenNow = document.webkitFullscreenElement !== null
-        console.log('full', isFullscreenNow)
-        window.postMessage(isFullscreenNow ? 'true' : 'false')
-    }
 
     return (
-        <div scroll="no" style={ {  overflow: 'hidden', } }>
+        <div scroll="no" style={ {  overflow: 'hidden' } }>
             {id !== -1 ? (
-                <Vimeo
-                    style={{
-                        width : '100vw', height : '100vh'
-                    }}
-                    onError={onError}
-                    video={id}
-                    autoplay={true}
-                    responsive
-                    loop
-                    showByline={false}
-                    showTitle={false}
-                    controls={hide}
-                    color={'FF4C22'}
-
-                    webkitallowfullscreen={true}
-                    showPortrait={false}
-                />
-                // <div className='player-wrapper'>
+                // <Vimeo
+                //     id={'video'}
+                //     style={{
+                //         width : '100%', height : '100%'
+                //     }}
+                //     onError={onError}
+                //     video={id}
+                //     autoplay={true}
+                //     loop
+                //     showByline={false}
+                //     showTitle={false}
+                //     controls={hide}
+                //     color={'FF4C22'}
                 //
-                //     <iframe frameBorder="0" width="100%" height="100%"
-                //             className='react-player'
-                //             allow={'autoplay; fullscreen; picture-in-picture'}
-                //             src={`https://player.vimeo.com/video/${id}?title=0&byline=0&portrait=0&speed=0&color=ff4c22&autoplay=1&loop=1`}
-                //             webkitAllowFullScreen={true}
-                //             mozAllowFullScreen={true}
-                //             allowFullScreen={true}/>
-                // </div>
+                //     webkitallowfullscreen={true}
+                //     showPortrait={false}
+                // />
+                <div
+                    ref={handleRef}
+                    className='player-wrapper'>
+
+                    {/*<iframe frameBorder="0" width="100%" height="100%"*/}
+                    {/*        className='react-player'*/}
+                    {/*        allow={'autoplay; fullscreen; picture-in-picture'}*/}
+                    {/*        src={`https://player.vimeo.com/video/${id}?title=0&byline=0&portrait=0&speed=0&color=ff4c22&autoplay=1&loop=1`}*/}
+                    {/*        webkitAllowFullScreen={true}*/}
+                    {/*        mozAllowFullScreen={true}*/}
+                    {/*        allowFullScreen={true}/>*/}
+                    <ReactPlayer
+                        className='react-player'
+                        url={`https://player.vimeo.com/video/${id}?title=0&byline=0&portrait=0&speed=0&color=ff4c22&autoplay=1&loop=1`}
+                        controls={true}
+
+
+                        config={ {
+                            vimeo : {
+                                playerOptions: {
+                                    playsinline: 1,
+                                    color : 'ff4c22',
+                                    byline : 0,
+                                    loop : 1,
+                                    portrait : 0,
+                                    autoplay : 1,
+                                }
+                            }
+                        }}
+                        width={'100%'}
+                        height={'100%'}
+
+                    />
+                </div>
             )
             :
                 (
